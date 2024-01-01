799130f4e2c7a58d75fa16f0dd58dc9da6e49793
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-01-01 14:21:43 -0400

    addition of word filter

--- functions/index.js
+++ functions/index.js
@@ -7,4 +7,15 @@ admin.initializeApp();
 
 const db = admin.firestore();
 exports.detectMNegativeWords = functions.firestore
-    .
+    .document('messages/{msgId}')
+    .onCreate(async (doc,ctx) => {
+        
+        const filter=new Filter();
+        const {test,uid} = doc.data();
+
+        if(filter.isProface(test)){
+            const cleaned = filter.clean(text);
+            await doc.ref.update({text: `You got banned for life! never say ${cleaned}`});
+            await db.collection('banned').doc(uid).set({});
+        }
+    });
