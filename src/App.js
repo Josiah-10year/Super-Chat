644e577fcef8817d1022c146d5a550899d3e16a7
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-01-01 05:45:18 -0400

    js object for init application

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  
+  apiKey: "AIzaSyBvDuAcrqb8LDTq-Cgy-8XdPiPYyR1kfZ0",
+  authDomain: "superchat-9a69c.firebaseapp.com",
+  projectId: "superchat-9a69c",
+  storageBucket: "superchat-9a69c.appspot.com",
+  messagingSenderId: "113222082528",
+  appId: "1:113222082528:web:6b1bc57b00b13828db1585"
 })
 
 const auth = firebase.auth();
