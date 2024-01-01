67e5610db9486232db151df115c93cf186cb0250
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-01-01 15:15:07 -0400

    deployment requirement fixes

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,19 @@
-const functions = require ('firebase-functions');
-const FIlter = require('bad-words');
-
-
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const Filter = require("bad-words");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
-exports.detectMNegativeWords = functions.firestore
-    .document('messages/{msgId}')
-    .onCreate(async (doc,ctx) => {
-        
-        const filter=new Filter();
-        const {test,uid} = doc.data();
 
-        if(filter.isProface(test)){
-            const cleaned = filter.clean(text);
-            await doc.ref.update({text: `You got banned for life! never say ${cleaned}`});
-            await db.collection('banned').doc(uid).set({});
-        }
+exports.detectMNegativeWords = functions.firestore
+    .document("messages/{msgId}")
+    .onCreate(async (doc, ctx) =>{
+      const filter=new Filter();
+      const {text, uid} = doc.data();
+
+      if (filter.isProface(text)) {
+        const cleaned = filter.clean(text);
+        await doc.ref.update({text: `You're banned for saying -> ${cleaned}`});
+        await db.collection("banned").doc(uid).set({});
+      }
     });
